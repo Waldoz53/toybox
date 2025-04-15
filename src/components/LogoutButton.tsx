6c24cb47254e0329119a6f1cddc7425a8de30de0
Waleed Rawasia <middul.dk@gmail.com>
2025-04-15 14:51:53 -0600

    added loading effect on logout

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,15 +1,19 @@
 'use client'
 import { logOut } from "@/app/actions"
+import useLoading from "@/app/context/LoadingContext"
 import { useUser } from "@/app/context/UserContext"
 import { useRouter } from "next/navigation"
 
 export default function LogoutButton() {
   const { setUser } = useUser()
+  const { setLoading } = useLoading()
   const router = useRouter()
 
   async function handleLogout() {
+    setLoading(true)
     await logOut()
     setUser(null)
+    setLoading(false)
     router.push('/')
   }
 
