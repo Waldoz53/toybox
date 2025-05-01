2b6d731a51cb90b4ec1348513a1449faa0a05931
Waleed Rawasia <middul.dk@gmail.com>
2025-05-01 15:00:18 -0600

    update blog for testing phase 1

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -6,10 +6,11 @@ export default function Blog() {
       <h2>Blog</h2>
       <div className="blog-section">
         <h3>Latest updates only</h3>
-        <p>April 25, 2025</p>
+        <p>May 1, 2025</p>
         <ul>
-          <li>Big CSS + code cleanup</li>
-          <li>Renamed /post route to /add</li>
+          <li>Official testing begins!</li>
+          <li>Added username validation so usernames are unique</li>
+          <li>Added email validation so the format should be standardized</li>
         </ul>
       </div>
 
@@ -26,10 +27,6 @@ export default function Blog() {
           <li>Turn delete buttons into icons for less chunky looking elements</li>
           <li>Optionally rating on a scale of 1-10 to your posts</li>
           <li>Show likes + comments icons on home page posts</li>
-          <li>
-            Validate emails, usernames (so that theyre unique), post titles, descriptions, etc so my
-            database doesnt get absolutely fricked
-          </li>
           <li>Follow/unfollow people then...</li>
           <li>On the home page you can switch between a following/all posts tab</li>
           <li>Profile setting to limit who can comment on your posts</li>
